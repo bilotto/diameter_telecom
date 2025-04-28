0a1e64c9b995f4841510a8aef5bceda8f62fec94
Fabio <bilotto@github.com>
2025-04-27 23:41:14 -0300

    Refactor DiameterSession class and introduce session types. Removed subscriber from DiameterSession constructor, added set_subscriber method. Created SySession and RxSession classes extending DiameterSession. Updated GxSession to manage framed IP addresses and added new methods for setting attributes.

--- src/diameter_telecom/diameter_session.py
+++ src/diameter_telecom/diameter_session.py
@@ -1,29 +1,26 @@
 from .subscriber import Subscriber
 from .diameter_message import DiameterMessage, Message
+from .constants import *
 from typing import List
 import time
 
 class DiameterSession:
-    subscriber: Subscriber
     session_id: str
     active: bool
     messages: List[DiameterMessage]
     start_time: str
     end_time: str
 
-    def __init__(self, subscriber: Subscriber, session_id: str):
-        if not isinstance(subscriber, Subscriber):
-            raise ValueError("Subscriber must be an instance of Subscriber")
+    def __init__(self, session_id: str):
         if not isinstance(session_id, str):
-            raise ValueError("session_id must be a string")
-        self.subscriber = subscriber
+            raise ValueError("session_id must be a string") 
         self.session_id = session_id
         self.active = False
         self.error = False
         self.messages = []
-        #
         self.start_time = None
         self.end_time = None
+        self.subscriber = None
 
     def __hash__(self) -> int:
         return hash(self.session_id)
@@ -58,8 +55,13 @@ class DiameterSession:
             raise ValueError("message must be an instance of Message or DiameterMessage")
         self.messages.append(diameter_message)
 
-    def get_messages(self):
+    def get_messages(self) -> List[DiameterMessage]:
         return self.messages
+    
+    def set_subscriber(self, subscriber: Subscriber):
+        if not isinstance(subscriber, Subscriber):
+            raise ValueError("subscriber must be an instance of Subscriber")
+        self.subscriber = subscriber
 
     @property
     def last_message(self):
@@ -79,18 +81,62 @@ class DiameterSession:
             return int(time.time() - float(self.start_time))
         return None
 
+class SySession(DiameterSession):
+    session_id: str
+    gx_session_id: str
+
+    def __init__(self, session_id: str):
+        super().__init__(session_id)
+        self.gx_session_id = None
+
+    def set_gx_session_id(self, gx_session_id: str):
+        self.gx_session_id = gx_session_id
+
+class RxSession(DiameterSession):
+    session_id: str
+
+    def __init__(self, session_id: str):
+        super().__init__(session_id)
+        self.gx_session_id = None
+
+    def set_gx_session_id(self, gx_session_id: str):
+        self.gx_session_id = gx_session_id
+
 
 class GxSession(DiameterSession):
     session_id: str
-    framed_ip_address: str
+    _framed_ip_address: str
+    _framed_ipv6_prefix: str
     apn: str
+    mcc_mnc: str
 
-    def __init__(self,
-                 subscriber,
-                 session_id: str,
-                 framed_ip_address: str,
-                 apn: str = None,):
-        super().__init__(subscriber, session_id)
-        self.framed_ip_address = framed_ip_address
+    def __init__(self, session_id: str):
+        super().__init__(session_id)
+        self._framed_ip_address = None
+        self._framed_ipv6_prefix = None
+        self.apn = None
+        self.mcc_mnc = None
+
+    @property
+    def framed_ip_address(self):
+        return self._framed_ip_address or self._framed_ipv6_prefix
+
+    def set_mcc_mnc(self, mcc_mnc: str):
+        self.mcc_mnc = mcc_mnc
+
+    def set_framed_ip_address(self, framed_ip_address: str):
+        self._framed_ip_address = framed_ip_address
+
+    def set_framed_ipv6_prefix(self, framed_ipv6_prefix: str):
+        self._framed_ipv6_prefix = framed_ipv6_prefix
+
+    def set_apn(self, apn: str):
         self.apn = apn
-        
+
+    def add_message(self, message: DiameterMessage):
+        super().add_message(message)
+        if message.name == CCR_I:
+            if message.timestamp:
+                self.set_start_time(message.timestamp)
+            else:
+                self.start()
