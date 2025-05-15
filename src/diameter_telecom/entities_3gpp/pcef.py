a03c00b1441f8cd8583554352419f5bf6318cfd1
Fabio Bilotto <fbilotto@amdocs.com>
2025-05-14 21:14:49 -0300

    Refactor DiameterEntity subclasses for improved initialization and peer management
    - Updated AF, DSC, and PCEF classes to remove inheritance from DiameterEntity and streamline their constructors.
    - Introduced methods for managing peers and realms, enhancing clarity in peer management.
    - Added properties for accessing peers and realms by application type, improving organization.
    - Implemented logging for better traceability during application startup and message handling.

--- src/diameter_telecom/entities_3gpp/pcef.py
+++ src/diameter_telecom/entities_3gpp/pcef.py
@@ -1,45 +1,88 @@
 from typing import List, Dict, Callable, Optional
-from ..diameter.helpers import Node
-from ._entity import DiameterEntity
+from ..diameter.helpers import Node, Peer, create_node
 from ..diameter.handle_request import handle_request_gx
-from ..subscriber import Subscriber
-#
 from ..diameter.constants import *
-from diameter.message.commands import CreditControlRequest
 from diameter_telecom.diameter.app import GxApplication
-from ..apn import APN, ip_to_bytes, bytes_to_ip
+import logging
+logger = logging.getLogger(__name__)
 
-class PCEF(DiameterEntity):
+def node_peer_uri(node: Node):
+    if node.tcp_port:
+        return f"aaa://{node.origin_host}:{node.tcp_port};transport=tcp"
+    elif node.sctp_port:
+        return f"aaa://{node.origin_host}:{node.sctp_port};transport=sctp"
+    else:
+        raise ValueError(f"Node {node.origin_host} has no TCP or SCTP port")
+
+
+class PCEF():
     def __init__(self, origin_host: str, realm_name: str,
                  ip_addresses: List[str],
                  tcp_port: int = None, sctp_port: int = None,
                  vendor_ids: List[int] = None,
-                 max_threads: int = 1,
-                 request_handler: Callable = handle_request_gx):
-        super().__init__(origin_host=origin_host, realm_name=realm_name, ip_addresses=ip_addresses, tcp_port=tcp_port, sctp_port=sctp_port, vendor_ids=vendor_ids)
-        self.gx_app: GxApplication = GxApplication(max_threads=max_threads, request_handler=request_handler)
+                 ):
+        self.origin_host = origin_host
+        self.realm_name = realm_name
+        self.ip_addresses = ip_addresses
+        self.tcp_port = tcp_port
+        self.sctp_port = sctp_port
+        self.vendor_ids = vendor_ids
+        self.node: Node = create_node(origin_host, realm_name, ip_addresses, tcp_port, sctp_port, vendor_ids)
+        self.all_peers: Dict[str, List[Peer]] = {}
+        self.all_realms: Dict[str, List[str]] = {}
+        self._setup_app_ran = False
+        self.add_gx_realm(self.realm_name)
 
-    def start_gx_session(self, subscriber: Subscriber):
-        """Start a Gx session for a subscriber"""
-        ccr_i = CreditControlRequest()
-        ccr_i.auth_application_id = APP_3GPP_GX
-        ccr_i.session_id = self.node.session_generator.next_id()
-        ccr_i.origin_host = self.origin_host.encode()
-        ccr_i.origin_realm = self.realm_name.encode()
-        ccr_i.destination_realm = self.realm_name.encode()
-        ccr_i.cc_request_type = E_CC_REQUEST_TYPE_INITIAL_REQUEST
-        ccr_i.cc_request_number = 0
-        ccr_i.framed_ip_address = ip_to_bytes(subscriber.apn.ip_queue.get_ip())
-        ccr_i.subscription_id = subscriber.subscription_id()
-        ccr_i.bearer_usage = E_BEARER_USAGE_GENERAL
-        ccr_i.called_station_id = subscriber.apn.apn
-        ccr_i.rat_type = E_RAT_TYPE_EUTRAN
-        ccr_i.ip_can_type = E_IP_CAN_TYPE_3GPP_EPS
-        ccr_i.network_request_support = E_NETWORK_REQUEST_SUPPORT_NETWORK_REQUEST_SUPPORTED
-        ccr_i.event_trigger = []
-        ccr_i.event_trigger.append(E_EVENT_TRIGGER_RAT_CHANGE)
-        ccr_i.event_trigger.append(E_EVENT_TRIGGER_QOS_CHANGE)
-        ccr_i.event_trigger.append(E_EVENT_TRIGGER_USAGE_REPORT)
-        ccr_i.event_trigger.append(E_EVENT_TRIGGER_IP_CAN_CHANGE)
-        ccr_i.event_trigger.append(E_EVENT_TRIGGER_PLMN_CHANGE)
-        self.gx_app.send_request(ccr_i)
+    @property
+    def peer_uri(self):
+        return f"aaa://{self.origin_host}:{self.tcp_port};transport=tcp"
+    
+    @property
+    def gx_peers(self):
+        return self.all_peers.get(APP_3GPP_GX, [])
+    
+    @property
+    def gx_realms(self):
+        return self.all_realms.get(APP_3GPP_GX, [])
+
+    def add_node_as_peer(self, node_: Node, app_id: str, initiate_connection: bool = False):
+        if app_id not in self.all_peers:
+            self.all_peers[app_id] = []
+        self.all_peers[app_id].append(self.node.add_peer(node_peer_uri(node_), node_.realm_name, node_.ip_addresses, is_persistent=initiate_connection))
+
+    def add_realm(self, app_id: str, realm_name: str):
+        if app_id not in self.all_realms:
+            self.all_realms[app_id] = []
+        if realm_name not in self.all_realms[app_id]:
+            self.all_realms[app_id].append(realm_name)
+
+    def add_gx_realm(self, realm_name: str):
+        if realm_name not in self.gx_realms:
+            self.add_realm(APP_3GPP_GX, realm_name)
+
+    def setup_app(self, app_id: int, max_threads: int = 1, request_handler: Callable = handle_request_gx):
+        if app_id == APP_3GPP_GX:
+            self.gx_app = GxApplication(max_threads=max_threads, request_handler=request_handler)
+            self.node.add_application(self.gx_app, self.gx_peers, self.gx_realms)
+        self._setup_app_ran = True
+
+    def setup_gx_app(self, max_threads: int = 1, request_handler: Callable = handle_request_gx):
+        self.setup_app(APP_3GPP_GX, max_threads, request_handler)
+
+    def start(self):
+        # if self.gx_app and self.gx_peers:
+        #     logger.info(f"Starting GxApplication in node {self.node.origin_host} with {len(self.gx_peers)} peers and {len(self.gx_realms)} realms. Realms: {self.gx_realms}")
+        #     self.node.add_application(self.gx_app, self.gx_peers, self.gx_realms)
+        if not self._setup_app_ran:
+            logger.error("setup_app must be called before start")
+            return
+        self.node.start()
+
+    def stop(self):
+        if self.node._started:
+            self.node.stop()
+
+    def wait_for_ready(self):
+        for app in self.node.applications:
+            app.wait_for_ready()
+
