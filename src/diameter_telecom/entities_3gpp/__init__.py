1be24bf55fbdb097e905ad4c33a0afb8e1469dad
Fabio Bilotto <fbilotto@amdocs.com>
2025-05-14 21:16:16 -0300

    Update __all__ export in 3GPP module to include OCS class

--- src/diameter_telecom/entities_3gpp/__init__.py
+++ src/diameter_telecom/entities_3gpp/__init__.py
@@ -1,6 +1,7 @@
 from .pcef import PCEF
 from .pcrf import PCRF
 from .af import AF
+from .ocs import OCS
 from .dsc import DSC
 
-__all__ = ["PCEF", "PCRF", "AF", "DSC"]
+__all__ = ["PCEF", "PCRF", "AF", "OCS", "DSC"]
