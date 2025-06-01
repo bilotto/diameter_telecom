335a091bd8ee7bfd6fe4752b5569e05bfa155b6f
Fabio Bilotto <fbilotto@amdocs.com>
2025-06-01 02:55:04 -0300

    Refactor GxSession message handling to differentiate between request and response processing

--- src/diameter_telecom/diameter/session/gx.py
+++ src/diameter_telecom/diameter/session/gx.py
@@ -16,28 +16,33 @@ class GxSession(DiameterSession):
         diameter_message = super().add_message(message)
         if not diameter_message:
             return
-        if diameter_message.name == CCR_I:
-            if diameter_message.timestamp:
-                self.start_time = diameter_message.timestamp
-            if diameter_message.message.framed_ip_address:
-                self.framed_ip_address = diameter_message.message.framed_ip_address
-            if diameter_message.message.framed_ipv6_prefix:
-                self.framed_ipv6_prefix = diameter_message.message.framed_ipv6_prefix
-            if diameter_message.message.called_station_id:
-                self.called_station_id = diameter_message.message.called_station_id
-            if diameter_message.message.sgsn_mcc_mnc:
-                self.sgsn_mcc_mnc = diameter_message.message.sgsn_mcc_mnc
-            if diameter_message.message.subscription_id:
-                msisdn, imsi, sip_uri, nai, private = parse_subscription_id(diameter_message.message.subscription_id)
-                if not self.subscriber:
-                    self.subscriber = Subscriber(msisdn=msisdn, imsi=imsi)
-        elif diameter_message.name == CCA_I:
-            if diameter_message.timestamp and diameter_message.result_code == E_RESULT_CODE_DIAMETER_SUCCESS:
-                self.active = True
-        elif diameter_message.name == CCA_T:
-            if diameter_message.timestamp and diameter_message.result_code == E_RESULT_CODE_DIAMETER_SUCCESS:
-                self.active = False
-                self.ended = True
-                self.end_time = diameter_message.timestamp
+        if diameter_message.is_request:
+            if diameter_message.name == CCR_I:
+                if diameter_message.timestamp:
+                    self.start_time = diameter_message.timestamp
+                if diameter_message.message.framed_ip_address:
+                    self.framed_ip_address = diameter_message.message.framed_ip_address
+                if diameter_message.message.framed_ipv6_prefix:
+                    self.framed_ipv6_prefix = diameter_message.message.framed_ipv6_prefix
+                if diameter_message.message.called_station_id:
+                    self.called_station_id = diameter_message.message.called_station_id
+                if diameter_message.message.sgsn_mcc_mnc:
+                    self.sgsn_mcc_mnc = diameter_message.message.sgsn_mcc_mnc
+                if diameter_message.message.subscription_id:
+                    msisdn, imsi, sip_uri, nai, private = parse_subscription_id(diameter_message.message.subscription_id)
+                    if not self.subscriber:
+                        self.subscriber = Subscriber(msisdn=msisdn, imsi=imsi)
+        else:
+            if diameter_message.result_code and diameter_message.result_code != E_RESULT_CODE_DIAMETER_SUCCESS:
+                self.error = True
+            if diameter_message.name == CCA_I:
+                if diameter_message.timestamp and diameter_message.result_code == E_RESULT_CODE_DIAMETER_SUCCESS:
+                    self.active = True
+            elif diameter_message.name == CCA_T:
+                if diameter_message.timestamp:
+                    self.active = False
+                    self.ended = True
+                    self.end_time = diameter_message.timestamp
+        #
         if not diameter_message.subscriber and self.subscriber:
             diameter_message.subscriber = self.subscriber
