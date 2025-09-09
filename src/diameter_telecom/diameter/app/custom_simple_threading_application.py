6eb65b3951f394fad9c5bdd25b6beea41f5ef7c5
Fabio Bilotto <Fabio.Bilotto@amdocs.com>
2025-09-09 17:55:24 -0300

    Add SessionManager integration in CustomSimpleThreadingApplication to handle Diameter message processing. This improves session management and enhances message handling clarity.

--- src/diameter_telecom/diameter/app/custom_simple_threading_application.py
+++ src/diameter_telecom/diameter/app/custom_simple_threading_application.py
@@ -7,6 +7,7 @@ from typing import Dict
 import logging
 logger = logging.getLogger(__name__)
 import time
+from ..session_manager import SessionManager
 
 class CustomSimpleThreadingApplication(SimpleThreadingApplication):
     def __init__(self, application_id,
@@ -18,6 +19,7 @@ class CustomSimpleThreadingApplication(SimpleThreadingApplication):
         super().__init__(application_id, is_acct_application, is_auth_application, max_threads, request_handler)
         self.sessions: Dict[str, DiameterSession] = {}
         self.subscribers: Dict[str, Subscriber] = {}
+        self.session_manager = SessionManager()
 
     def remove_session(self, session_id):
         if session_id in self.sessions:
@@ -30,6 +32,7 @@ class CustomSimpleThreadingApplication(SimpleThreadingApplication):
 
     def send_request_custom(self, diameter_message: DiameterMessage, timeout=10):
         diameter_message.timestamp = time.time()
+        self.session_manager.process_diameter_message(diameter_message)
         logger.info(f"\n{diameter_message.dump()}")
         answer = self.send_request(diameter_message.message, timeout=timeout)
         diameter_message_answer = DiameterMessage(answer)
@@ -37,4 +40,5 @@ class CustomSimpleThreadingApplication(SimpleThreadingApplication):
         if diameter_message_answer.result_code != E_RESULT_CODE_DIAMETER_SUCCESS:
             logger.error(f"Answer with error: \n {diameter_message_answer}")
         logger.info(f"\n{diameter_message_answer.dump()}")
+        self.session_manager.process_diameter_message(diameter_message_answer)
         return diameter_message_answer
